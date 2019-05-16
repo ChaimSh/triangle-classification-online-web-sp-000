cec5926eeee94b64c05d4b425cbac5794126c24b
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-05-16 02:19:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,9 @@
 class Triangle
-  # write code here
+  attr_accessor :triangle_side1, :triangle_side2, :triangle_side3
+
+  def initialize(triangle_side1, triangle_side2, triangle_side3)
+   @triangle_side1 = triangle_side1
+   @triangle_side2 = triangle_side2
+   @triangle_side3 = triangle_side3
+  end
 end
